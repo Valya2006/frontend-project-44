73fe89ba869c9aa17846e4f6c3e278dd76f7320c
valya <valia2006@DESKTOP-KJRGD7H.localdomain>
2025-08-03 14:58:07 +0300

    The eighth step

---
+++ src/games/ccc.js
@@ -0,0 +1,3 @@
+
+
+console.log(replaceAt([1, 3, 5, 7, 9], 0).join(' '))
--- src/games/gcd.js
+++ src/games/gcd.js
@@ -4,7 +4,7 @@ const descriptionGcd = 'Find the greatest common divisor of given numbers.';
 
 const generateRoundGcd = () => {
 	let num1 = getRandomNumber(1, 50);
-	let num2 = getRandomNumber(1, 50);
+	let num2 = getRandomNumber(2, 50);
 	const question = `${num1} ${num2}`;
 	while (num2 !== 0) {
 		[num1, num2] = [num2, num1 % num2]
